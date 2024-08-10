95d887df5dd45c5a995ead2eb7af6ddea8aa3837
meenachinmay <chinmayanand896@gmail.com>
2024-08-11 00:36:03 +0900

    added email service to ms package

---
+++ email-service/src/main/kotlin/com/meenachinmay/email_service/EmailServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.meenachinmay.email_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmailServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmailServiceApplication>(*args)
+}
